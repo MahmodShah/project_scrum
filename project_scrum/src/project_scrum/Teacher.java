fa362f39cc4cebb925702dd7151f7c4d1164b067
Ahmad Shah Habib <ahmad.shahbcs@gmail.com>
2022-09-04 19:34:14 +0200

    ADDED MULTIPLE THINGS

--- project_scrum/src/project_scrum/Teacher.java
+++ project_scrum/src/project_scrum/Teacher.java
@@ -5,6 +5,12 @@ public class Teacher {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("How are you man");
+		System.out.println("How are you man");
+		System.out.println("How are you man");
+		System.out.println("How are you man");
+		System.out.println("How are you man");
+		System.out.println("How are you man");
+		
 	}
 
 }
